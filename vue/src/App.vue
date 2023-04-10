063b429367b880c165409b5bb176bf0521d1219c
Nayilah Roper <nayilahroper@gmail.com>
2023-04-10 15:42:30 -0400

    VueComponents3

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,6 +1,6 @@
 <template class="page">
   <div id="app">
-    <div id="nav">
+    <div id="nav" class="nav">
       <router-link v-bind:to="{ name: 'home' }"
         ><img
           src="https://i.imgur.com/rBJDZci.png"
@@ -11,12 +11,16 @@
         v-if="$store.state.token != ''"
         >Logout</router-link
       >
+      <button class="adoptBtn">
       <router-link v-bind:to="{ name: 'adopt' }" class="petList">
         Adopt</router-link
       >
+      </button>
+      <button class="volBtn">
       <router-link v-bind:to="{ name: 'volunteer' }" class="volunteerList">
         Volunteer
       </router-link>
+      </button>
     </div>
     <router-view />
   </div>
@@ -29,25 +33,25 @@
 
 <style>
 
-
+.nav {
+  display: flex;
+  border: dashed red;
+  justify-content: space-between;
+}
 
 .image {
   padding: 25px;
-  width: 10%;
+  width: 10vh;
   height: 10vh;
 }
 
 .petList {
-  padding-left: 75vh;
-  padding-right: 75vh;
   color: white;
   font-weight: bold;
   font-family: "Poppins", sans-serif;
 }
 
 .volunteerList {
-  padding-left: 75vh;
-  padding-right: 75vh;
   color: white;
   font-weight: bold;
   font-family: "Poppins", sans-serif;
@@ -58,8 +62,21 @@ html,
 body {
   background-image: url("https://i.imgur.com/d534hFp.png");
   background-size: 100%;
-
+  background-color: #A19581;
   background-repeat: no-repeat;
+}
+.adoptBtn{
+
+  border-radius: 16px;
+  background-color: #de854e;
+  padding: 15px 20px;
+  
+}
+
+.volBtn{
+   border-radius: 16px;
+  background-color: #de854e;
+  padding: 15px 50px;
 }
 </style>
 
