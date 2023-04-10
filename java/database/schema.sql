01603b6851c4deb97ef441e39e5af5aa5f1a5c2b
Josh Coffey <joshcoffey1996@gmail.com>
2023-04-10 10:58:54 -0400

    created tables in schema.sql

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,5 +1,9 @@
 BEGIN TRANSACTION;
 
+DROP TABLE IF EXISTS photo;
+DROP TABLE IF EXISTS pets;
+
+DROP TABLE IF EXISTS application;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -7,7 +11,42 @@ CREATE TABLE users (
 	username varchar(50) NOT NULL UNIQUE,
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
+	first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	DOB date NOT NULL,
+	phone_number varchar(10) NOT NULL,
+	email_address varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+CREATE TABLE application (
+	application_id SERIAL PRIMARY KEY,
+	user_id int NOT NULL,
+	application_date date NOT NULL,
+	status varchar(20),
+	
+	CONSTRAINT fk_user_id FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE pets(
+	pet_id SERIAL PRIMARY KEY,
+	name varchar(20) NOT NULL,
+	DOB date NOT NULL,
+	breed varchar(20) NOT NULL,
+	color varchar(20) NOT NULL,
+	sex varchar(6) NOt NULL,
+	status varchar(30) NOT NULL,
+	is_vaccinated boolean DEFAULT false NOT NULL,
+	entry_date date DEFAULT NOW() NOT NULL,
+	adoption_date date DEFAULT NULL,
+	description varchar(500) NOT NULL
+);
+
+CREATE TABLE photo(
+	photo_id SERIAL PRIMARY KEY,
+	pet_id int NOT NULL,
+	source varchar(500) NOT NULL,
+	
+	CONSTRAINT fk_photo FOREIGN KEY(pet_id) REFERENCES pets(pet_id)
+);
 COMMIT TRANSACTION;
