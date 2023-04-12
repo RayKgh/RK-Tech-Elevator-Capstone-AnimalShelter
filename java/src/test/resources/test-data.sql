21cfdbb57fba06390d151261bbdb158a766423da
Josh Coffey <joshcoffey1996@gmail.com>
2023-04-11 21:19:57 -0400

    added null checks to daos

--- java/src/test/resources/test-data.sql
+++ java/src/test/resources/test-data.sql
@@ -5,10 +5,10 @@ INSERT INTO users (username,password_hash,role) VALUES ('user2','user2','ROLE_US
 INSERT INTO users (username,password_hash,role) VALUES ('user3','user3','ROLE_USER');
 
 INSERT INTO pets(pet_name, DOB, breed, color, sex, adoption_status, is_vaccinated, entry_date, adoption_date, pet_description)
-VALUES ('name1', '2011-11-11', 'test','test','test','test',false,'2011-11-11','2011-11-11','test');
+VALUES ('name1', '2011-11-11', 'test','test','test','Adopted',false,'2011-11-11','2011-11-11','test');
 
 INSERT INTO pets(pet_name, DOB, breed, color, sex, adoption_status, is_vaccinated, entry_date, adoption_date, pet_description)
-VALUES ('name2','2011-11-11', 'test','test','test','test',false,'2011-11-11','2011-11-11','test');
+VALUES ('name2','2011-11-11', 'test','test','test','Adopted',false,'2011-11-11','2011-11-11','test');
 
 INSERT INTO photos(pet_id,source,description)
 VALUES(1,'test1','test description');
