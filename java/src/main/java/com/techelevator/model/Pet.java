c8335d8332d8809321c270639d6beccf538c34e7
Josh Coffey <joshcoffey1996@gmail.com>
2023-04-11 21:28:49 -0400

    removed time calculators. for some reason it broke things

--- java/src/main/java/com/techelevator/model/Pet.java
+++ java/src/main/java/com/techelevator/model/Pet.java
@@ -34,19 +34,19 @@ public class Pet {
     }
 
     //ignores leap years but should be good enough. I don't think sloths live that long.
-    public int getAge(){
-        LocalDate thisDate = LocalDate.now();
-        int days = thisDate.getYear()*365+thisDate.getDayOfYear();
-        int daysAlive = DOB.getYear()*365+DOB.getDayOfYear();
-        return (days-daysAlive)/365;
-    }
-
-    public int getTimeInShelter(){
-        LocalDate thisDate = LocalDate.now();
-        int days = thisDate.getYear()*365+thisDate.getDayOfYear();
-        int daysAlive = entryDate.getYear()*365+entryDate.getDayOfYear();
-        return (days-daysAlive)/365;
-    }
+//    public int getAge() {
+//        LocalDate thisDate = LocalDate.now();
+//        int days = thisDate.getYear() * 365 + thisDate.getDayOfYear();
+//        int daysAlive = DOB.getYear() * 365 + DOB.getDayOfYear();
+//        return (days - daysAlive) / 365;
+//    }
+//
+//    public int getTimeInShelter() {
+//        LocalDate thisDate = LocalDate.now();
+//        int days = thisDate.getYear() * 365 + thisDate.getDayOfYear();
+//        int daysAlive = entryDate.getYear() * 365 + entryDate.getDayOfYear();
+//        return (days - daysAlive) / 365;
+//    }
 
 
     public int getPetID() {
@@ -110,9 +110,9 @@ public class Pet {
     }
 
     public void setAdoptionStatus(String adoptionStatus) {
-        if(adoptionStatus.equals("Available") || adoptionStatus.equals("Adopted")) {
+        if (adoptionStatus.equals("Available") || adoptionStatus.equals("Adopted")) {
             this.adoptionStatus = adoptionStatus;
-        } else{
+        } else {
             throw new IllegalArgumentException("adoption status should be either 'Available' or 'Adopted'");
         }
     }
--- java/src/test/java/com/techelevator/dao/PetTests.java
+++ java/src/test/java/com/techelevator/dao/PetTests.java
@@ -11,21 +11,21 @@ public class PetTests {
 
     private Pet pet = new Pet();
 
-    @Test
-    public void testPetAges(){
-        this.pet.setDOB(LocalDate.of(1996,7,3));
-        Assert.assertEquals(26,pet.getAge());
-        this.pet.setDOB(LocalDate.of(2001,9,26));
-        Assert.assertEquals(21,pet.getAge());
-        this.pet.setDOB(LocalDate.of(2000,1,1));
-        Assert.assertEquals(23,pet.getAge());
-    }
-
-    @Test
-    public void testHowLongPetHasBeenInShelter(){
-        this.pet.setEntryDate(LocalDate.of(1996,7,3));
-        Assert.assertEquals(26,pet.getTimeInShelter());
-    }
+//    @Test
+//    public void testPetAges(){
+//        this.pet.setDOB(LocalDate.of(1996,7,3));
+//        Assert.assertEquals(26,pet.getAge());
+//        this.pet.setDOB(LocalDate.of(2001,9,26));
+//        Assert.assertEquals(21,pet.getAge());
+//        this.pet.setDOB(LocalDate.of(2000,1,1));
+//        Assert.assertEquals(23,pet.getAge());
+//    }
+//
+//    @Test
+//    public void testHowLongPetHasBeenInShelter(){
+//        this.pet.setEntryDate(LocalDate.of(1996,7,3));
+//        Assert.assertEquals(26,pet.getTimeInShelter());
+//    }
 
 
 }
