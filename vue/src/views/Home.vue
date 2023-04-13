d8baa8e23ba82f3ac8699b444ef1c4f3e3ed8035
Jake Arnett <Jake.Arnett@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[a[D[D[D[D[j[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D>
2023-04-13 08:56:33 -0400

    added responsive and mobile backgrounds

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -59,37 +59,26 @@ export default {
   font-size: 30px;
 }
 
-@media screen and (max-width: 1650px) {
+
+
+@media screen and (max-width: 1680px) {
 
   h1{
     font-size: 90px;
   }
-   h2{
-     font-size: 30px;
-     width: 500px
-   }
-}
-
-@media screen and (max-width: 1400px) {
-
-  h1{
-    font-size: 70px;
-    margin-bottom: -10px;
-  }
 
   h2{
      font-size: 30px;
-     width: 500px;
-     margin: 0;
-    }
-
+     width: 400px
+  }
+  
   .browse-btn {
     border-radius: 50px;
     border: transparent;
     background-color: #de854e;
     text-transform: uppercase;
     padding: 12px 25px;
-    margin: 5px 0;
+    margin: 10px 20px;
     }
 
   .btn-text {
@@ -99,7 +88,68 @@ export default {
   text-decoration: none;
   text-transform: uppercase;
   font-size: 20px;
-}
+  }
 }
 
+@media screen and (max-width: 1400px) {
+
+  .home{
+     margin: 100px 30px 0 50px;
+    /* background-color: tomato; */
+    width: 60%;
+  }
+
+  h1{
+    font-size: 70px;
+    margin-bottom: -10px;
+    margin-left: -10px
+  }
+
+  h2{
+     font-size: 30px;
+     width: 400px;
+     margin: 0;
+    }
+
+  .browse-btn {
+    margin: 10px 0px;
+    }
+  
+} 
+
+@media screen and (max-width: 900px) {
+
+  .home{
+    margin: 150px 30px 0 80px;
+    /* background-color: tomato; */
+    width: 60%;
+  }
+
+  h1{
+    font-size: 50px;
+    margin: -10px 0px;
+    /* background-color: pink; */
+  }
+
+  h2{
+     font-size: 30px;
+     width: 400px;
+     margin: 0;
+    }
+}
+
+@media screen and (max-width: 800px) {
+  .home{
+    /* background-color: magenta; */
+    margin: 100px 20px;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .home{
+    /* background-color: magenta; */
+    margin: 100px 20px;
+    width: 90%;
+  }
+}
 </style>
