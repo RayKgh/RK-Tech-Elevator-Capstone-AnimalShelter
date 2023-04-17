852fb53841affd3790643490c3f613b6986c4be3
Jake Arnett <Jake.Arnett@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[a[D[D[D[D[j[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D>
2023-04-17 15:10:57 -0400

    directory now show users in a table

--- vue/src/components/UserTable.vue
+++ vue/src/components/UserTable.vue
@@ -6,7 +6,6 @@
         <th>Last Name</th>
         <th>Email</th>
         <th>Phone Number</th>
-        <th>Role</th>
       </thead>
       <tbody>
         <tr v-for="user in users" v-bind:key="user.userId">
@@ -14,7 +13,6 @@
           <td>{{ user.lastName }}</td>
           <td>{{ user.email }}</td>
           <td>{{ user.phone }}</td>
-          <td>{{ user.authorities[0] }}</td>
         </tr>
       </tbody>
     </table>
@@ -37,5 +35,67 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+h1 {
+  font-size: 50px;
+  margin-right: 20px;
+  background-color: red;
+}
+
+table {
+  margin: 0 auto;
+  background-color: #335137;
+  border-collapse: collapse;
+  border-radius: 30px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.2);
+  width: 1200px;
+}
+
+th {
+  color: #f2ebe6;
+  padding: 20px 30px 15px 30px;
+  font-size: 15px;
+  text-transform: uppercase;
+  text-align: left;
+  border-radius: 30px;
+}
+
+tr {
+  color: #335137;
+}
+
+tr:nth-child(even) {
+  background-color: #e1dbd6;
+}
+tr:nth-child(odd) {
+  background-color: #f2ebe6;
+}
+
+tr:last-child td:first-child {
+  border-bottom-left-radius: 30px;
+}
+
+tr:last-child td:last-child {
+  border-bottom-right-radius: 30px;
+}
+
+td {
+  border: none;
+  padding: 15px 0px;
+  padding-left: 25px;
+}
+
+td.left {
+  padding-left: 30px;
+}
+
+td.has-icon {
+  text-align: center;
+}
+
+td i.fa-solid {
+  padding-left: 20px;
+  color: #3d7b51;
+  font-size: 25px;
+}
 </style>
